38efcb522b398550022d2b8c48927ca71df1083d
soc06212 <soc06212@gmail.com>
2022-01-09 21:26:01 +0900

    Feat(HomeFragment): HomeFragment 레이아웃 작업 (작업중)

--- app/src/main/java/com/hallym/hlth/fragments/HomeFragment.kt
+++ app/src/main/java/com/hallym/hlth/fragments/HomeFragment.kt
@@ -1,10 +1,9 @@
 package com.hallym.hlth.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.hallym.hlth.MainActivity
 import com.hallym.hlth.R
 import com.hallym.hlth.databinding.FragmentHomeBinding
 
@@ -24,5 +23,33 @@ class HomeFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() = HomeFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as MainActivity).setSupportActionBar(binding.toolbarHome)
+        (requireActivity() as MainActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_notification -> {
+                // TODO: Show notification
+            }
+
+            R.id.action_account -> {
+                // TODO: Show account settings
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
     }
 }
