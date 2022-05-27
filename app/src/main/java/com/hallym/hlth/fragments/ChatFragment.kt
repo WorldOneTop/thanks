e41124a8212ad306b9761d9ebe0c756a0ee4ac6c
dlwpdlf147 <dlwpdlf147@naver.com>
2022-05-27 20:10:28 +0900

    채팅 탭에서 채팅방 추가 버튼 생성

--- app/src/main/java/com/hallym/hlth/fragments/ChatFragment.kt
+++ app/src/main/java/com/hallym/hlth/fragments/ChatFragment.kt
@@ -1,37 +1,44 @@
 package com.hallym.hlth.fragments
 
+//import com.hallym.hlth.function.ChatStorage
+import android.animation.ObjectAnimator
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.VERTICAL
 import com.hallym.hlth.ChatInActivity
 import com.hallym.hlth.MainActivity
-import com.hallym.hlth.NotificationActivity
+import com.hallym.hlth.MenteeManageData
 import com.hallym.hlth.adapters.ChatAdapter
-import com.hallym.hlth.adapters.HomeAdapter
 import com.hallym.hlth.databinding.FragmentChatBinding
 import com.hallym.hlth.function.ChatController
 import com.hallym.hlth.function.ChatDB
 import com.hallym.hlth.function.Query
-//import com.hallym.hlth.function.ChatStorage
-import com.hallym.hlth.models.Chatting
-import java.util.*
+import kotlinx.android.synthetic.main.fragment_chat.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 class ChatFragment : Fragment() {
 
     private lateinit var binding: FragmentChatBinding
     private lateinit var adapter: ChatAdapter
+    private lateinit var listViewAdapter: ArrayAdapter<String>
+    private lateinit var listItemName: ArrayList<String>
+    private lateinit var listItemId: ArrayList<Int>
+    private var addButtonIsOpen: Boolean = false
+
     private val mBroadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             if(intent.getStringExtra("category") == "send"){
@@ -64,6 +71,7 @@ class ChatFragment : Fragment() {
 
 
         initRecyclerView()
+        initAddView()
     }
 
     private fun initRecyclerView() {
@@ -79,7 +87,48 @@ class ChatFragment : Fragment() {
             intent.putExtra("userName",userName)
             startActivity(intent)
         }
+    }
+    private fun initAddView(){
+        listItemName = ArrayList()
+        listItemId = ArrayList()
 
+        listViewAdapter = ArrayAdapter<String>(requireContext(),
+            android.R.layout.simple_list_item_1, listItemName)
+        binding.chatAddList.adapter = listViewAdapter
+
+        Query().getMenteesDoc{
+            val data = it.getJSONArray("data")
+
+            for(i in 0 until data.length()){
+                listItemName.add(data.getJSONObject(i).getString("userId__name"))
+                listItemId.add(data.getJSONObject(i).getInt("userId"))
+            }
+            CoroutineScope(Dispatchers.Main).launch {
+                listViewAdapter.notifyDataSetChanged()
+            }
+        }
+
+        ObjectAnimator.ofFloat(chatAddList, "translationY", 200f).start()
+
+        binding.chatAddButton.setOnClickListener{
+           if (addButtonIsOpen) {
+               ObjectAnimator.ofFloat(chatAddList, "translationY", 200f).start()
+               ObjectAnimator.ofFloat(chatAddList,"alpha",1f,0f).start()
+               ObjectAnimator.ofFloat(it, View.ROTATION, 45f, 0f).start()
+           } else {
+               ObjectAnimator.ofFloat(chatAddList, "translationY", 0f).start()
+               ObjectAnimator.ofFloat(chatAddList,"alpha",0f,1f).start()
+               ObjectAnimator.ofFloat(it, View.ROTATION, 0f, 45f).start()
+           }
+           addButtonIsOpen = !addButtonIsOpen
+       }
+
+        binding.chatAddList.setOnItemClickListener{ _, _, position, _ ->
+            val intent = Intent(requireContext(), ChatInActivity::class.java)
+            intent.putExtra("userId", listItemId[position])
+            intent.putExtra("userName", listItemName[position])
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
@@ -92,5 +141,8 @@ class ChatFragment : Fragment() {
     override fun onStop() {
         super.onStop()
         LocalBroadcastManager.getInstance(requireContext()).unregisterReceiver(mBroadcastReceiver)
+        if(addButtonIsOpen){
+            binding.chatAddButton.callOnClick()
+        }
     }
 }
